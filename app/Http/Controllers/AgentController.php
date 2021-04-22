4342d3139959e3602dce36b9a31f1a437f451028
kamal <mustapakamalkml@gmail.com>
2021-04-22 15:45:26 +0800

    agent save

--- app/Http/Controllers/AgentController.php
+++ app/Http/Controllers/AgentController.php
@@ -57,32 +57,60 @@ class AgentController extends Controller
         if (is_null($request->id)) {
             $msg = [
                 'nama.required' => 'Nama Tidak Boleh Kosong',
+                'nik.required' => 'NIK Tidak Boleh Kosong',
+                'nik.numeric' => 'NIK Harus Angka',
+                'nik.unique' => 'NIK Sudah Terdaftar, NIK Tidak Bisa di Daftarkan 2 Kali',
+                'tempat_lahir.required' => 'Tempat Lahir Tidak Boleh Kosong',
+                'tanggal_lahir.required' => 'Tanggal Lahir Tidak Boleh Kosong',
                 'telepon.required' => 'Telepon Tidak Boleh Kosong',
+                'telepon.min' => 'Telepon Tidak Boleh Kurang dari 5 Angka',
+                'telepon.numeric' => 'Telepon Hanya Boleh Diisi Angka',
+                'telepon.max' => 'Telepon Hanya Boleh Maksimal 15 Angka',
                 'email.required' => 'Email Tidak Boleh Kosong',
-                'nama_pj.required' => 'Nama Penanggungjawab Tidak Boleh Kosong',
-                'nomor_pj.required' => 'Nomor Penanggungjawab Tidak Boleh Kosong',
+                'email.unique' => 'Email Sudah Pernah Terdaftar',
+                'alamat.required' => 'Alamat Tidak Boleh Kosong',
+                'pekerjaan.required' => 'Pekerjaan Tidak Boleh Kosong',
+                'validasi.required' => 'Anda Belum Menyetujui Syarat dan Ketentuan Kebijakan Privasi',
             ];
             $validator = Validator::make($request->all(), [
                 'nama' => 'required',
-                'telepon' => 'required',
-                'email' => 'required',
-                'nama_pj' => 'required',
-                'nomor_pj' => 'required',
+                'nik' => 'required|numeric|unique:pmb_agent,nik',
+                'tempat_lahir' => 'required',
+                'tanggal_lahir' => 'required',
+                'telepon' => 'required|min:5|max:15|numeric',
+                'email' => 'required|email|unique:pmb_agent,email',
+                'alamat' => 'required',
+                'pekerjaan' => 'required',
+                'validasi' => 'required',
             ], $msg);
         } else {
             $msg = [
                 'nama.required' => 'Nama Tidak Boleh Kosong',
+                'nik.required' => 'NIK Tidak Boleh Kosong',
+                'nik.numeric' => 'NIK Harus Angka',
+                'nik.unique' => 'NIK Sudah Terdaftar, NIK Tidak Bisa di Daftarkan 2 Kali',
+                'tempat_lahir.required' => 'Tempat Lahir Tidak Boleh Kosong',
+                'tanggal_lahir.required' => 'Tanggal Lahir Tidak Boleh Kosong',
                 'telepon.required' => 'Telepon Tidak Boleh Kosong',
+                'telepon.min' => 'Telepon Tidak Boleh Kurang dari 5 Angka',
+                'telepon.numeric' => 'Telepon Hanya Boleh Diisi Angka',
+                'telepon.max' => 'Telepon Hanya Boleh Maksimal 15 Angka',
                 'email.required' => 'Email Tidak Boleh Kosong',
-                'nama_pj.required' => 'Nama Penanggungjawab Tidak Boleh Kosong',
-                'nomor_pj.required' => 'Nomor Penanggungjawab Tidak Boleh Kosong',
+                'email.unique' => 'Email Sudah Pernah Terdaftar',
+                'alamat.required' => 'Alamat Tidak Boleh Kosong',
+                'pekerjaan.required' => 'Pekerjaan Tidak Boleh Kosong',
+                'validasi.required' => 'Anda Belum Menyetujui Syarat dan Ketentuan Kebijakan Privasi',
             ];
             $validator = Validator::make($request->all(), [
                 'nama' => 'required',
-                'telepon' => 'required',
-                'email' => 'required',
-                'nama_pj' => 'required',
-                'nomor_pj' => 'required',
+                'nik' => 'required|numeric|unique:pmb_agent,nik',
+                'tempat_lahir' => 'required',
+                'tanggal_lahir' => 'required',
+                'telepon' => 'required|min:5|max:15|numeric',
+                'email' => 'required|email|unique:pmb_agent,email',
+                'alamat' => 'required',
+                'pekerjaan' => 'required',
+                'validasi' => 'required',
             ], $msg);
         }
 
@@ -100,11 +128,23 @@ class AgentController extends Controller
 
             Agent::create([
                 'name' => $request->nama,
+                'nik' => $request->nik,
+                'tempat_lahir' => $request->tempat_lahir,
+                'tanggal_lahir' => $request->tanggal_lahir,
                 'telepon' => $request->telepon,
                 'email' => $request->email,
+                'jenis_kelamin' => $request->jenis_kelamin,
+                'agama' => $request->agama,
+                'status_perkawinan' => $request->status_perkawinan,
+                'jabatan' => $request->jabatan,
+                'provinsi' => $request->provinsi,
+                'kabupaten' => $request->kabupaten,
+                'kecamatan' => $request->kecamatan,
+                'kelurahan' => $request->kelurahan,
                 'alamat' => $request->alamat,
-                'nama_pj' => $request->nama_pj,
-                'nomor_pj' => $request->nomor_pj,
+                'nama_lembaga' => $request->nama_lembaga,
+                'telepon_kantor' => $request->telepon_kantor,
+                'alamat_kantor' => $request->alamat_kantor,
                 'kode_ucb' => '03',
                 'nomor_daftar' => $jml,
                 'kode_agent' => $depan . $jml . '-03',
@@ -116,8 +156,8 @@ class AgentController extends Controller
             return redirect($this->url1);
         }
         // dd($validator->errors()->all());
-        Alert::error('tite', 'Error');
-        return redirect($this->url);
+        Alert::error('title', 'Error');
+        return redirect($this->url1);
     }
 
     public function individu_store(Request $request)
@@ -127,34 +167,62 @@ class AgentController extends Controller
             $msg = [
                 'nama.required' => 'Nama Tidak Boleh Kosong',
                 'nik.required' => 'NIK Tidak Boleh Kosong',
+                'nik.numeric' => 'NIK Harus Angka',
+                'nik.unique' => 'NIK Sudah Terdaftar, NIK Tidak Bisa di Daftarkan 2 Kali',
+                'tempat_lahir.required' => 'Tempat Lahir Tidak Boleh Kosong',
+                'tanggal_lahir.required' => 'Tanggal Lahir Tidak Boleh Kosong',
                 'telepon.required' => 'Telepon Tidak Boleh Kosong',
+                'telepon.min' => 'Telepon Tidak Boleh Kurang dari 5 Angka',
+                'telepon.numeric' => 'Telepon Hanya Boleh Diisi Angka',
+                'telepon.max' => 'Telepon Hanya Boleh Maksimal 15 Angka',
                 'email.required' => 'Email Tidak Boleh Kosong',
+                'email.unique' => 'Email Sudah Pernah Terdaftar',
                 'alamat.required' => 'Alamat Tidak Boleh Kosong',
+                'pekerjaan.required' => 'Pekerjaan Tidak Boleh Kosong',
+                'validasi.required' => 'Anda Belum Menyetujui Syarat dan Ketentuan Kebijakan Privasi',
             ];
             $validator = Validator::make($request->all(), [
                 'nama' => 'required',
-                'nik' => 'required',
-                'telepon' => 'required',
-                'email' => 'required',
+                'nik' => 'required|numeric|unique:pmb_agent,nik',
+                'tempat_lahir' => 'required',
+                'tanggal_lahir' => 'required',
+                'telepon' => 'required|min:5|max:15|numeric',
+                'email' => 'required|email|unique:pmb_agent,email',
                 'alamat' => 'required',
+                'pekerjaan' => 'required',
+                'validasi' => 'required',
             ], $msg);
         } else {
             $msg = [
                 'nama.required' => 'Nama Tidak Boleh Kosong',
                 'nik.required' => 'NIK Tidak Boleh Kosong',
+                'nik.numeric' => 'NIK Harus Angka',
+                'nik.unique' => 'NIK Sudah Terdaftar, NIK Tidak Bisa di Daftarkan 2 Kali',
+                'tempat_lahir.required' => 'Tempat Lahir Tidak Boleh Kosong',
+                'tanggal_lahir.required' => 'Tanggal Lahir Tidak Boleh Kosong',
                 'telepon.required' => 'Telepon Tidak Boleh Kosong',
+                'telepon.min' => 'Telepon Tidak Boleh Kurang dari 5 Angka',
+                'telepon.numeric' => 'Telepon Hanya Boleh Diisi Angka',
+                'telepon.max' => 'Telepon Hanya Boleh Maksimal 15 Angka',
                 'email.required' => 'Email Tidak Boleh Kosong',
+                'email.unique' => 'Email Sudah Pernah Terdaftar',
                 'alamat.required' => 'Alamat Tidak Boleh Kosong',
+                'pekerjaan.required' => 'Pekerjaan Tidak Boleh Kosong',
+                'validasi.required' => 'Anda Belum Menyetujui Syarat dan Ketentuan Kebijakan Privasi',
             ];
             $validator = Validator::make($request->all(), [
                 'nama' => 'required',
-                'nik' => 'required',
-                'telepon' => 'required',
-                'email' => 'required',
+                'nik' => 'required|numeric|unique:pmb_agent,nik',
+                'tempat_lahir' => 'required',
+                'tanggal_lahir' => 'required',
+                'telepon' => 'required|min:5|max:15|numeric',
+                'email' => 'required|email|unique:pmb_agent,email',
                 'alamat' => 'required',
+                'pekerjaan' => 'required',
+                'validasi' => 'required',
             ], $msg);
         }
-
+        // dd($validator->errors()->all());
 
         if ($validator->passes()) {
 
@@ -174,6 +242,11 @@ class AgentController extends Controller
                 'tanggal_lahir' => $request->tanggal_lahir,
                 'telepon' => $request->telepon,
                 'email' => $request->email,
+                'jenis_kelamin' => $request->jenis_kelamin,
+                'agama' => $request->agama,
+                'status_perkawinan' => $request->status_perkawinan,
+                'pekerjaan' => $request->pekerjaan,
+                'instansi' => $request->instansi,
                 'provinsi' => $request->provinsi,
                 'kabupaten' => $request->kabupaten,
                 'kecamatan' => $request->kecamatan,
@@ -189,9 +262,9 @@ class AgentController extends Controller
 
             return redirect($this->url2);
         }
-        // dd($validator->errors()->all());
-        Alert::error('tite', 'Error');
-        return redirect($this->url);
+
+        Alert::error('title', $validator->errors()->all());
+        return redirect($this->url2);
     }
 
 
